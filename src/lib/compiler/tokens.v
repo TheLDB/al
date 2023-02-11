248065a3caa0c2981bff3fa2feee88c9c1cb9c6b
Alistair Smith <hi@alistair.sh>
2023-02-11 22:48:47 +0000

    token to string

--- src/lib/compiler/tokens.v
+++ src/lib/compiler/tokens.v
@@ -86,3 +86,62 @@ pub enum AtKind {
 	file_path // @path – Gets the path of the current file
 	line // @line – Gets the line number of the current line where the token appears
 }
+
+pub fn kind_to_string(kind Kind) string {
+	return match kind {
+		.eof { 'eof' }
+		.literal_symbol { 'literal_symbol' }
+		.literal_number { 'literal_number' }
+		.literal_string { 'literal_string' }
+		// .literal_string_interpolation { 'literal_string_interpolation' }
+		.math_plus { 'math_plus' }
+		.math_minus { 'math_minus' }
+		.math_mul { 'math_mul' }
+		.math_div { 'math_div' }
+		.math_mod { 'math_mod' }
+		.math_incr { 'math_incr' }
+		.math_decr { 'math_decr' }
+		.reassign { 'reassign' }
+		.declare { 'declare' }
+		.logical_and { 'logical_and' }
+		.logical_or { 'logical_or' }
+		.logical_not { 'logical_not' }
+		.bitwise_and { 'bitwise_and' }
+		.bitwise_or { 'bitwise_or' }
+		.bitwise_xor { 'bitwise_xor' }
+		.bitwise_not { 'bitwise_not' }
+		.comp_equal { 'comp_equal' }
+		.comp_not_equal { 'comp_not_equal' }
+		.comp_greater_than { 'comp_greater_than' }
+		.comp_less_than { 'comp_less_than' }
+		.comp_greater_than_or_equal { 'comp_greater_than_or_equal' }
+		.comp_less_than_or_equal { 'comp_less_than_or_equal' }
+		.ctrl_if { 'ctrl_if' }
+		.ctrl_else { 'ctrl_else' }
+		.ctrl_loop { 'ctrl_loop' }
+		.kw_function { 'kw_function' }
+		.kw_return { 'kw_return' }
+		.kw_break { 'kw_break' }
+		.kw_continue { 'kw_continue' }
+		.kw_import { 'kw_import' }
+		.kw_from { 'kw_from' }
+		.kw_true { 'kw_true' }
+		.kw_false { 'kw_false' }
+		.kw_null { 'kw_null' }
+		.kw_assert { 'kw_assert' }
+		.kw_export { 'kw_export' }
+		.punc_comma { 'punc_comma' }
+		.punc_colon { 'punc_colon' }
+		.punc_dot { 'punc_dot' }
+		.punc_dotdot { 'punc_dotdot' }
+		.punc_ellipsis { 'punc_ellipsis' }
+		.punc_open_paren { 'punc_open_paren' }
+		.punc_close_paren { 'punc_close_paren' }
+		.punc_open_brace { 'punc_open_brace' }
+		.punc_close_brace { 'punc_close_brace' }
+		.punc_open_bracket { 'punc_open_bracket' }
+		.punc_close_bracket { 'punc_close_bracket' }
+		.punc_at { 'punc_at' }
+		else { panic('Invalid token kind: ${kind}') }
+	}
+}
